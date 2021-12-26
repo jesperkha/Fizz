b5f1dcc26ff85248cb3555d56aa7415575a67174
jesperkha <hammerjesp@gmail.com>
2021-12-27 00:27:39 +0100

    Updated build_all dest path

--- dev/build_all.sh
+++ dev/build_all.sh
@@ -2,5 +2,5 @@ set -e
 [ ! -d "./bin" ] && mkdir bin
 python build_lib.py
 go vet .
-GOOS=windows GOARCH=amd64 go build -o ./export/fizz.exe main.go run.go
-GOOS=linux GOARCH=amd64 go build -o ./export/fizz main.go run.go
+GOOS=windows GOARCH=amd64 go build -o ./bin/fizz.exe .
+GOOS=linux GOARCH=amd64 go build -o ./bin/fizz .
