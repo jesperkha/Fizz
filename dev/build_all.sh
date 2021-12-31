19700db0d9eb2a1b54ce285b3904028adede351a
jesperkha <hammerjesp@gmail.com>
2021-12-31 18:39:38 +0100

    Build fix

--- dev/build_all.sh
+++ dev/build_all.sh
@@ -4,5 +4,4 @@ python lib/build.py
 go vet .
 
 GOOS=windows GOARCH=amd64 go build -o ./bin/fizz.exe .
-GOOS=linux GOARCH=amd64 go build -o ./bin/fizz_lin .
-GOOS=ios GOARCH=arm64 go build -o ./bin/fizz_ios .
+GOOS=linux GOARCH=amd64 go build -o ./bin/fizz .
